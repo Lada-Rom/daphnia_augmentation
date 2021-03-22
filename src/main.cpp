35b72ca0273fbd7b6880561fdfecac6af31ab031
Lada-Rom <doodleztoon@gmail.com>
2021-03-22 20:22:11 +0300

    Drawing the bubble with two methods

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
+
     std::string source_filename = "../../data/Male_Day13.avi";
     cv::VideoCapture cap(source_filename);
 
@@ -12,16 +13,27 @@ int main() {
     }
 
     cv::Mat frame;
+    cv::Mat bubble = cv::imread("../../data/bubble.png");
+    cv::Mat bubble2 = cv::imread("../../data/bubble2.png");
+    cv::Mat light = cv::imread("../../data/light.png");
+    cv::Rect roi(1100, 150, bubble.cols, bubble.rows);
+
     while (true) {
         cap >> frame;
         if (frame.empty())
             break;
+
+//        frame(roi) -= bubble;
+//        frame(roi) += light;
+        cv::addWeighted(frame(roi), 0.5, bubble2, 0.5, 0, frame(roi));
+
         cv::imshow("Frame", frame);
-        char key = (char)cv::waitKey(1);
+        char key = (char)cv::waitKey(100);
         if (key == 27)
             break;
     }
 
     cap.release();
     cv::destroyAllWindows();
+
 }
