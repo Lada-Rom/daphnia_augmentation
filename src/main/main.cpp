b440063f080e8b272fdbeaed770d509652f90220
Lada-Rom <doodleztoon@gmail.com>
2021-03-28 15:37:41 +0300

    Code refactored

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,10 +1,98 @@
 #include <iostream>
-#include <string>
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+const size_t num_bubbles{ 4 };
+
+const std::string main_dir = "../../../data/";
+const std::string masks_subdir = "masks/";
+const std::string video_subdir = "videos/";
+
+const std::string main_name = "bubble";
+const std::string black_label_name = "_black";
+const std::string white_label_name = "_white";
+const std::string main_ext = ".png";
+
+std::vector<cv::Mat> getKit(const std::string& label_name) {
+    std::vector<cv::Mat> kit;
+    std::string fullname = main_dir + masks_subdir + main_name;
+
+    for (size_t i{}; i < num_bubbles; ++i) {
+        kit.push_back(cv::imread(
+            fullname + std::to_string(i) + label_name + main_ext));
+    }
+
+    return kit;
+}
+
+void showBubble(cv::Mat& frame, 
+    const cv::Mat& black, const cv::Mat& white,
+    double alpha, double beta, size_t x, size_t y) {
+    cv::Rect roi(x, y, black.cols, black.rows);
+    frame(roi) -= alpha * black;
+    frame(roi) += beta * white;
+}
+
+void showAllBubbles(cv::Mat& frame,
+    const std::vector<cv::Mat>& black_kit, 
+    const std::vector<cv::Mat>& white_kit, 
+    size_t step, const std::vector<size_t>& borders, bool draw_border = true) {
+
+    size_t left_border = borders[0];
+    size_t right_border = frame.cols - borders[1];
+    size_t upper_border = borders[2];
+    size_t lower_border = frame.rows - borders[3];
+
+    if (draw_border) {
+        cv::Rect rect = cv::Rect(
+            borders[0], borders[2],
+            right_border - left_border, lower_border - upper_border);
+        cv::rectangle(frame, rect, cv::Scalar{ 0, 0, 0 });
+    }
+
+    //if bubbles too much, it can multyplies
+    size_t x = left_border, y = upper_border;
+    for (size_t i{}; i < num_bubbles; ++i) {
+        showBubble(frame, black_kit[i], white_kit[i], 0.4, 0.2, x, y);
+        if (x + step + black_kit[i].cols < right_border) {
+            x += step;
+        }
+        else {
+            x = left_border;
+            if (y + step < lower_border)
+                y += step;
+            else
+                continue;
+        }
+    }
+}
+
+void playVideoWithBubbles(cv::VideoCapture& cap, size_t step) {
+    cv::Mat frame;
+
+    //borders: left, right, up, down
+    std::vector<size_t> borders{110, 120, 60, 60};
+    std::vector<cv::Mat> bubble_black_kit = getKit(black_label_name);
+    std::vector<cv::Mat> bubble_white_kit = getKit(white_label_name);
+
+    while (true) {
+        cap >> frame;
+        if (frame.empty())
+            break;
+
+        showAllBubbles(frame, bubble_black_kit, bubble_white_kit, step, borders);
+
+        cv::imshow("Frame", frame);
+        char key = (char)cv::waitKey(10);
+        if (key == 27)
+            break;
+    }
+}
 
 int main() {
-
-    std::string source_filename = "../../data/Male_Day13.avi";
+    std::string video_name = "Male_Day13.avi";
+    std::string source_filename = main_dir + video_subdir + video_name;
     cv::VideoCapture cap(source_filename);
 
     if (!cap.isOpened()) {
@@ -12,26 +100,7 @@ int main() {
         return -1;
     }
 
-    cv::Mat frame;
-    cv::Mat bubble = cv::imread("../../data/bubble.png");
-    cv::Mat bubble2 = cv::imread("../../data/bubble2.png");
-    cv::Mat light = cv::imread("../../data/light.png");
-    cv::Rect roi(1100, 150, bubble.cols, bubble.rows);
-
-    while (true) {
-        cap >> frame;
-        if (frame.empty())
-            break;
-
-//        frame(roi) -= bubble;
-//        frame(roi) += light;
-        cv::addWeighted(frame(roi), 0.5, bubble2, 0.5, 0, frame(roi));
-
-        cv::imshow("Frame", frame);
-        char key = (char)cv::waitKey(1);
-        if (key == 27)
-            break;
-    }
+    playVideoWithBubbles(cap, 150);
 
     cap.release();
     cv::destroyAllWindows();
--- src/processing/processing.cpp
+++ src/processing/processing.cpp
@@ -1,7 +1,97 @@
 #include <iostream>
-#include <string>
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+const size_t num_bubbles{ 4 };
+
+void showWindow(const cv::Mat& image, std::string name = "Image",
+    size_t x = 0, size_t y = 0) {
+    imshow(name, image);
+    cv::moveWindow(name, x, y);
+}
+
+void imshowStack(const std::string& name, size_t x, size_t y,
+    const cv::Mat& mat1, const cv::Mat& mat2, const cv::Mat& mat3, const cv::Mat& mat4) {
+    cv::Mat general = cv::Mat::zeros(mat1.rows, 4 * mat1.cols, CV_8UC1);
+    cv::Rect roi;
+
+    roi = cv::Rect(0 * mat1.cols, 0, mat1.cols, mat1.rows);
+    mat1.copyTo(general(roi));
+
+    roi = cv::Rect(1 * mat1.cols, 0, mat1.cols, mat1.rows);
+    mat2.copyTo(general(roi));
+
+    roi = cv::Rect(2 * mat1.cols, 0, mat1.cols, mat1.rows);
+    mat3.copyTo(general(roi));
+
+    roi = cv::Rect(3 * mat1.cols, 0, mat1.cols, mat1.rows);
+    mat4.copyTo(general(roi));
+
+    showWindow(general, name, x, y);
+}
+
+int calcMedian(const cv::Mat& mat) {
+    double sum{};
+    for (size_t i{}; i < mat.cols; ++i)
+        for (size_t j{}; j < mat.rows; ++j) {
+            sum += mat.at<uchar>(j, i);
+        }
+
+    return sum / (mat.cols * mat.rows);
+}
+
+void writeMasks(const cv::Mat& image,
+    const std::string& black_path, const std::string& white_path,
+    size_t x, size_t y, size_t index) {
+    cv::Mat neg_image = cv::Scalar{ 255 } - image;
+    double black_median = calcMedian(neg_image);
+    double white_median = calcMedian(image);
+
+    cv::Mat black_mask;
+    cv::threshold(neg_image, black_mask, black_median, 255, cv::ThresholdTypes::THRESH_TOZERO);
+    cv::GaussianBlur(black_mask, black_mask, cv::Size(3, 3), 0.9);
+    cv::imwrite(black_path, black_mask);
+
+    cv::Mat white_mask;
+    cv::threshold(image, white_mask, white_median + 20, 255, cv::ThresholdTypes::THRESH_TOZERO);
+    cv::imwrite(white_path, white_mask);
+
+    imshowStack("image" + std::to_string(index), x, y, image, neg_image, black_mask, white_mask);
+}
+
+std::vector<cv::Mat> getAllBubbles(const std::string& dir, const std::string& name, const std::string& ext) {
+    std::string fullname = dir + name;
+    std::vector<cv::Mat> kit;
+
+    for (size_t i{}; i < num_bubbles; ++i) {
+        kit.push_back(cv::imread(fullname + std::to_string(i) + ext, cv::IMREAD_GRAYSCALE));
+    }
+
+    return kit;
+}
+
+void writeAllMasks(const std::vector<cv::Mat> kit, const std::string& dir, const std::string& name,
+    const std::string& black_label_name, const std::string& white_label_name, const std::string& ext) {
+    std::string fullname = dir + name;
+
+    std::string black_name;
+    std::string white_name;
+    for (size_t i{}; i < num_bubbles; ++i) {
+        black_name = fullname + std::to_string(i) + black_label_name + ext;
+        white_name = fullname + std::to_string(i) + white_label_name + ext;
+        writeMasks(kit[i], black_name, white_name, 50, i * 75 + 50, i);
+    }
+}
 
 int main() {
+    const std::string src_dir = "../../../data/bubbles/";
+    const std::string dst_dir = "../../../data/masks/";
+    const std::string main_name = "bubble";
+    const std::string main_ext = ".png";
 
+    std::vector<cv::Mat> bubbles_kit = getAllBubbles(src_dir, main_name, main_ext);
+    writeAllMasks(bubbles_kit, dst_dir, main_name, "_black", "_white", main_ext);
+
+    cv::waitKey(0);
 }
